1809caa0ca8a7aa478362bd8320bf17aff9579f0
Ian Khor <ian@Ians-Mac-mini.local>
2024-09-05 22:59:36 +1000

    without tests

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -12,17 +12,10 @@ describe('<Todo />', () => {
     })
 
     it('shows loadings on initial rendering while fetching todo list', () => {
-        renderWithWrapper(<Todo />);
-        expect(screen.getByText(/loading/i)).toBeInTheDocument();
-        expect(screen.queryByText(/Item 1/i)).not.toBeInTheDocument();
+        
     })
 
     it('after loading, it shows Item 1', async () => {
-        renderWithWrapper(<Todo />);
-
-        expect(screen.getByText(/loading/i)).toBeInTheDocument();
-        expect(screen.queryByText(/Item 1/i)).not.toBeInTheDocument();
-        await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
-        expect(screen.queryByText(/Item 1/i)).toBeInTheDocument();
+        
     })
 });
--- src/Todo2.test.js
+++ src/Todo2.test.js
@@ -9,18 +9,5 @@ describe('<Todo />', () => {
     it('after loading, shows Item 2 after clicking on the To do button', async () => {
         renderWithWrapper(<Todo />);
 
-        expect(screen.getByText(/loading/i)).toBeInTheDocument();
-        expect(screen.queryByText(/Item 1/i)).not.toBeInTheDocument();
-
-        await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
-
-        expect(screen.queryByText(/Item 1/i)).toBeInTheDocument();
-        expect(screen.queryByText(/Item 2/i)).not.toBeInTheDocument();
-        
-        const toDoButton = screen.getByRole('button', { name: /add todo/i})
-        user.click(toDoButton)
-
-        await waitForElementToBeRemoved(() => screen.getByText(/Item 1/i))
-        expect(screen.queryByText(/Item 2/i)).toBeInTheDocument();
     })
 });
